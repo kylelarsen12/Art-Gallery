9378263e0f79100b87806ff6087418ebae80472e
unknown <kylelarsen12@gmail.com>
2022-08-18 16:14:03 -0400

    Set up store, data slice, and update app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import './App.css';
-import { useSelector, useDispatch } from 'react-redux'
+import "./App.css";
+import { useSelector, useDispatch, connect } from "react-redux";
+import {
+  clearData,
+  fetchData,
+  incrementId,
+  decrementId,
+  customId,
+} from "./features/dataSlice";
+import { useEffect } from "react";
 
-function App() {
+function App(props) {
   // your logic goes here!
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.data);
+
+  const renderImage = () => {
+    if (data.apiData) {
+      return <img src={data.apiData.primaryImage} alt={data.apiData.title} />;
+    } else {
+      return <p>Image goes here</p>;
+    }
+  };
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [props.objectId, dispatch]);
 
   return (
     <div className="App">
       <div>
-        <button onClick={() => {}}>Trigger Thunk</button>
-        <button onClick={() => {}}>Clear</button>
-        <button onClick={() => {}}>Next</button>
-        <button onClick={() => {}}>Back</button>
+        <button
+          onClick={() => {
+            dispatch();
+          }}
+        >
+          Trigger Thunk
+        </button>
+        <button
+          onClick={() => {
+            dispatch(clearData());
+          }}
+        >
+          Clear
+        </button>
+        <button
+          onClick={() => {
+            dispatch(incrementId());
+          }}
+        >
+          Next
+        </button>
+        <button
+          onClick={() => {
+            dispatch(decrementId());
+          }}
+        >
+          Back
+        </button>
       </div>
-      <input onChange={(e) => { }} />
+      <input
+        value={data.objectId}
+        onChange={(e) => {
+          dispatch(customId(Number(e.target.value)));
+        }}
+      />
       <div>
         {/* Once you have plugged everything in, render the image here! */}
+        {data.objectId}
+        {renderImage()}
       </div>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state, ownProps) => ({
+  objectId: state.data.objectId,
+});
+export default connect(mapStateToProps)(App);
---
+++ src/features/dataSlice.js
@@ -0,0 +1,45 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  objectId: 10245,
+  apiData: {},
+};
+
+export const dataSlice = createSlice({
+  name: "data",
+  initialState,
+  reducers: {
+    setData: (state, action) => {
+      return { ...state, apiData: action.payload };
+    },
+    clearData: () => {
+      return initialState;
+    },
+    customId: (state, action) => {
+      return { ...state, objectId: action.payload };
+    },
+    incrementId: (state) => {
+      return { ...state, objectId: state.objectId + 1 };
+    },
+    decrementId: (state) => {
+      return { ...state, objectId: state.objectId - 1 };
+    },
+  },
+});
+
+export const { setData, clearData, customId, incrementId, decrementId } =
+  dataSlice.actions;
+
+export const fetchData = () => {
+  const fetchThunk = async (dispatch, getState) => {
+    let state = getState();
+    const response = await fetch(
+      `https://collectionapi.metmuseum.org/public/collection/v1/objects/${state.data.objectId}`
+    );
+    const resData = await response.json();
+    dispatch(setData(resData));
+  };
+  return fetchThunk;
+};
+
+export default dataSlice.reducer;
